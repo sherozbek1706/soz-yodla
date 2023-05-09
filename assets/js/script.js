5c8ebbc932a58e10e329da005ae13c2142b7569e
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-05-09 05:33:23 +0500

    update version

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ let comboDiv = document.querySelector("#combo-div");
 let comboNum = document.querySelector("#combo-num");
 
 // VERSION
-let appVersion = "3.0.3";
+let appVersion = "3.0.4";
 
 let comboXG = 0;
 let isOne = true;
